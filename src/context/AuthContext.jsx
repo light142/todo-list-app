7cd325814bf571731d8694c64a208a1f45e203d2
Light <bluelight142@gmail.com>
2025-03-27 03:27:34 +1300

    init project and add user register function

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,28 @@
+// context/AuthContext.jsx
+import React, { createContext, useState, useEffect, useContext } from 'react';
+
+// Create context for authentication
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token); // Check if token exists in localStorage
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
